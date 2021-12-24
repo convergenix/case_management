536283c9384ffabb6c2d38cba2e8217542f5a9c3
mymi14s <mymi14s@hotmail.com>
2021-12-24 12:34:49 +0000

    Fix: Expense Claim, Invoice

---
+++ case_management/case_management/report/test_report/test_report.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2021, masonarmani38@gmail.com and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Test Report"] = {
+	"filters": [
+
+	]
+};
---
+++ case_management/case_management/report/test_report/test_report.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, masonarmani38@gmail.com and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+def execute(filters=None):
+	columns, data = [], []
+	return columns, data
